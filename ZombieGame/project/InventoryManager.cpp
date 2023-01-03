0e49f4d1080c94a0140e63b4face99cc7d166850
gaslam <44768183+gaslam@users.noreply.github.com>
2023-01-03 01:19:09 +0100

    Expand InventoryManager to detect weapons

--- ZombieGame/project/InventoryManager.cpp
+++ ZombieGame/project/InventoryManager.cpp
@@ -1,18 +1,57 @@
 #include "stdafx.h"
 #include "InventoryManager.h"
 #include "../inc/IExamInterface.h"
+#include <algorithm> 
 
+using namespace std;
+
+
+void InventoryManager::GrabItem(IExamInterface* pInterface, EntityInfo& entity, ItemInfo& item)
+{
+			//Once grabbed, you can add it to a specific inventory slot
+			//Slot must be empty
+			if (pInterface->Item_Grab(entity, item))
+			{
+				if (item.Type == eItemType::PISTOL || item.Type == eItemType::SHOTGUN)
+				{
+					m_CanUseWeapon = true;
+					m_WeaponSlots.push_back(m_InventorySlot);
+				}
+				pInterface->Inventory_AddItem(m_InventorySlot, item);
+			}
+}
 
 void InventoryManager::Update(IExamInterface* pInterface)
 {
 	HandleControls(pInterface);
 }
 
-ItemInfo InventoryManager::ReturnItemInfo(IExamInterface* pInterface)
+ItemInfo InventoryManager::ReturnPlayerSelectedItemInfo(IExamInterface* pInterface)
 {
-	ItemInfo info = {};
+	ItemInfo info{};
 	pInterface->Inventory_GetItem(m_InventorySlot, info);
 	return info;
+}
+
+void InventoryManager::UseItem(IExamInterface* pInterface)
+{
+	if (m_UseItem)
+	{
+		//Use an item (make sure there is an item at the given inventory slot)
+		pInterface->Inventory_UseItem(m_InventorySlot);
+	}
+}
+
+void InventoryManager::RemoveItem(IExamInterface* pInterface)
+{
+	if (m_RemoveItem)
+	{
+		//Remove an item from a inventory slot
+		pInterface->Inventory_RemoveItem(m_InventorySlot);
+		std::remove_if(m_WeaponSlots.begin(), m_WeaponSlots.end(), [&](int slot) {
+			return slot == m_InventorySlot;
+			});
+	}
 }
 
 void InventoryManager::HandleControls(IExamInterface* pInterface)
@@ -31,14 +70,22 @@ void InventoryManager::HandleControls(IExamInterface* pInterface)
 	}
 	else if (pInterface->Input_IsKeyboardKeyDown(Elite::eScancode_Q))
 	{
-		ItemInfo info = ReturnItemInfo(pInterface);
+		ItemInfo info = ReturnPlayerSelectedItemInfo(pInterface);
 		std::cout << (int)info.Type << std::endl;
 	}
 	else if (pInterface->Input_IsKeyboardKeyDown(Elite::eScancode_KP_Minus))
 	{
 		if (m_InventorySlot > 0)
 		{
+			m_CanUseWeapon = false;
 			--m_InventorySlot;
+			for (int slot : m_WeaponSlots)
+			{
+				if (slot == m_InventorySlot)
+				{
+					m_CanUseWeapon = true;
+				}
+			}
 			std::cout << "Current selected slot: SLOT(" << m_InventorySlot << ")\n";
 		}
 	}
@@ -46,7 +93,15 @@ void InventoryManager::HandleControls(IExamInterface* pInterface)
 	{
 		if (m_InventorySlot < m_TotalSlots)
 		{
+			m_CanUseWeapon = false;
 			++m_InventorySlot;
+			for (int slot : m_WeaponSlots)
+			{
+				if (slot == m_InventorySlot)
+				{
+					m_CanUseWeapon = true;
+				}
+			}
 			std::cout << "Current selected slot: SLOT(" << m_InventorySlot << ")\n";
 		}
 	}
--- ZombieGame/project/InventoryManager.h
+++ ZombieGame/project/InventoryManager.h
@@ -1,11 +1,16 @@
 #pragma once
 #include "IExamPlugin.h"
 #include "Exam_HelperStructs.h"
+#include <vector>
+
 class IExamInterface;
 class InventoryManager
 {
 public:
 	InventoryManager() = default;
+	void GrabItem(IExamInterface* pInterface, EntityInfo& entity, ItemInfo& item);
+	void UseItem(IExamInterface* pInterface);
+	void RemoveItem(IExamInterface* pInterface);
 	void SetUseItem(const bool canUseItem) { m_UseItem = canUseItem; };
 	void SetGrabItem(const bool canGrabItem) { m_GrabItem = canGrabItem; };
 	void SetRemoveItem(const bool canRemoveItem) { m_RemoveItem = canRemoveItem; };
@@ -13,15 +18,18 @@ public:
 	bool CanGrabItem() const { return m_GrabItem; };
 	bool CanUseItem() const { return m_UseItem; };
 	bool CanRemoveItem() const { return m_RemoveItem; };
+	bool PlayerUsesWeapon() const { return m_CanUseWeapon; };
 	UINT GetInventorySlot() const { return m_InventorySlot; };
 
-	ItemInfo ReturnItemInfo(IExamInterface* pInterface);
+	ItemInfo ReturnPlayerSelectedItemInfo(IExamInterface* pInterface);
 private:
 	void HandleControls(IExamInterface* pInterface);
 	bool m_GrabItem = false;
 	bool m_UseItem = false;
 	bool m_RemoveItem = false;
+	bool m_CanUseWeapon = false;
 	UINT m_InventorySlot = 0;
 	const int m_TotalSlots = 4;
+	std::vector<int> m_WeaponSlots{};
 };
 
